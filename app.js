cd84b78b0ba97edbb0d2582631f6ee481c29e709
yura <yur.stepanyuk@gmail.com>
2022-10-10 19:18:03 +0300

    style xs 70%

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 $('.img-slider').slick({
     dots: true,
     infinite: true,
@@ -7,3 +5,16 @@ $('.img-slider').slick({
     slidesToShow: 1,
     adaptiveHeight: true
   });
+
+let navMenu = document.querySelector('.menu');
+let navMobileMenu = document.querySelector('.nav-mobile-menu');
+
+navMenu.addEventListener("click", function() {
+  navMenu.classList.toggle('active-menu');
+  if(navMobileMenu.classList.contains('active-menu')) {
+    navMenu.classList.add('active-menu');
+  }
+  else {
+    navMenu.classList.remove('active-menu');
+  }
+});
